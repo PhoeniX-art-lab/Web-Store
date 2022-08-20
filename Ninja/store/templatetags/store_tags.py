b2ee3f6f964a2d52ae714297abf4347cea339140
PhoeniX <cool.max050203@yandex.ru>
2022-08-20 21:11:20 +0300

    Changed the display of information about the characteristics of the product

--- Ninja/store/templatetags/store_tags.py
+++ Ninja/store/templatetags/store_tags.py
@@ -1,4 +1,6 @@
 from django import template
+import markdown
+from django.utils.safestring import mark_safe
 
 from store.models import Category, Store
 
@@ -20,3 +22,8 @@ def get_menu():
     return [{"title": "Login", "url_name": "login"},
             {"title": "Support", "url_name": "support"},
             {"title": "About", "url_name": "about"}]
+
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
